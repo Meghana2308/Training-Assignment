7870bdbd4bff7686f875dadc5fe95e9b1d3ede9c
Meghana Damera <meghanadamerla2308@gmail.com>
2020-01-30 03:37:32 -0800

    Hibernate

---
+++ src/main/java/cts/model/EmpTest.java
@@ -0,0 +1,21 @@
+package cts.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmpTest {
+public static void main (String [] args) {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("cts");
+	EntityManager em = emf.createEntityManager();
+	em.getTransaction().begin();
+	Emp e= new Emp();
+	e.setEname("meghana");
+	e.setEname("sagar");
+	em.persist(e);
+	em.getTransaction().commit();
+	em.close();
+	emf.close();
+	System.out.println("Inserted ....");
+}
+}
